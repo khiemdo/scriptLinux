5f0f89a66a4c569a5a9cd5b743953ee127d82fba
khiemdo <khiemdo@hopetechnik.com>
2016-07-23 10:04:34 +0800

    change vsftpdInstall

--- scriptLinux/vsftpd/vsftpdInstall.py
+++ scriptLinux/vsftpd/vsftpdInstall.py
@@ -18,11 +18,13 @@ vsftpdConfigPath = ""
 def InstallVsfptd(logger):
     #installation here
     logger.info("Start installing vsftpd")
-    ret = subprocess.call("apt-get install vsftpd")
-    if(ret.find("error")!=-1):
-        logger.error("Error when installing vsftpd")
+    pc = subprocess.Popen("apt-get -y install vsftpd".split(),stdout = subprocess.PIPE)#todo
+    for line in pc.stdout:
+        logger.info(line.rstrip())
+        if(line.find("error")!=-1):
+            logger.error("Error when installing vsftpd")
+            sys.exit()
     logger.info("End installing vsftpd")
-
 def EditVsftpdConfig(logger,filePath):
     logger.info("Start editting vsftpd config")
     originFhd = open(filePath,'r+')
@@ -32,6 +34,7 @@ def EditVsftpdConfig(logger,filePath):
     tempFhd = open(tempAbsPath,'w+')
     lineNum = 0
     needleStr = ""
+    replacedStr = ""
 
     force_dot_files_Flag = 0
 
@@ -39,31 +42,35 @@ def EditVsftpdConfig(logger,filePath):
         lineNum += 1
         #Change anonymous_enable=YES to anonymous_enable=NO,
         needleStr = "anonymous_enable"
+        replacedStr="anonymous_enable=NO\n"#todo
         ret = line.find(needleStr)
         if(ret != -1):
-            logger.info("Found {} at line {}: \"{}\"".format(needleStr.strip(),lineNum,line.rstrip()))
-            ret = line.replace(line,"anonymous_enable=NO\n")
+            logger.info("Found {} at line {}: \"{}\"-->\"{}\"".format(needleStr.strip(),lineNum,line.rstrip(),replacedStr.rstrip()))#todo
+            line = line.replace(line,replacedStr)#todo
         #Uncomment local_enable=YES
         needleStr = "local_enable=YES"
-        ret = line.find(needleStr)
+        replacedStr="#local_enable=YES\n"
+        line_output = line.find(needleStr)
         if(ret != -1):
-            logger.info("Found {} at line {}: \"{}\"".format(needleStr.strip(),lineNum,line.rstrip()))
-            ret = line.replace(line,"#local_enable=YES\n")
+            logger.info("Found {} at line {}: \"{}\"-->\"{}\"".format(needleStr.strip(),lineNum,line.rstrip(),replacedStr.rstrip()))
+            line = line.replace(line,replacedStr)
         #Uncomment local_enable=YES
         needleStr = "write_enable=YES"
+        replacedStr="#write_enable=YES\n"#todo
         ret = line.find(needleStr)
         if(ret == 0):
-            logger.info("Found {} at line {}: \"{}\"".format(needleStr.strip(),lineNum,line.rstrip()))
-            ret = line.replace(line,"#write_enable=YES\n")
+            logger.info("Found {} at line {}: \"{}\"-->\"{}\"".format(needleStr.strip(),lineNum,line.rstrip(),replacedStr.rstrip()))
+            line = line.replace(line,replacedStr)
         elif (line[0] == '#' and ret == 1):
-            logger.info("Found {} at line {}: \"{}\"".format(needleStr.strip(),lineNum,line.rstrip()))
-            ret = line.replace(line,"#write_enable=YES\n")
+            logger.info("Found {} at line {}: \"{}\"-->\"{}\"".format(needleStr.strip(),lineNum,line.rstrip(),replacedStr.rstrip()))
+            line = line.replace(line,replacedStr)
         #Uncomment force_dot_files=YES\n
         needleStr = "force_dot_files"
+        replacedStr="force_dot_files=YES\n"
         ret = line.find(needleStr)
         if(ret != -1):
-            logger.info("Found {} at line {}: \"{}\"".format(needleStr.strip(),lineNum,line.rstrip()))
-            ret = line.replace(line,"force_dot_files=YES\n")
+            logger.info("Found {} at line {}: \"{}\"-->\"{}\"".format(needleStr.strip(),lineNum,line.rstrip(),replacedStr.rstrip()))
+            line = line.replace(line,replacedStr)
             force_dot_files_Flag = 1
         tempFhd.write(line)  
 
@@ -79,10 +86,8 @@ def EditVsftpdConfig(logger,filePath):
 
 
 if __name__ == "__main__":
-    logger = SetupLogger('vsftpdInstall',"./vsftpdInstall.log")
-
-#    filePath = path.join(script_dir, CONFIG_FILE_NAME) 
-
-    filePath = CONFIG_FILE_DIR + CONFIG_FILE_NAME
-    #InstallVsfptd(logger)
+    logger = BashHelper.SetupLogger('vsftpdInstall',"./vsftpdInstall.log")
+    filePath = path.join(SCRIPT_DIR, CONFIG_FILE_NAME) 
+#    filePath = CONFIG_FILE_DIR + CONFIG_FILE_NAME
+ #   InstallVsfptd(logger)
     EditVsftpdConfig(logger,filePath)
