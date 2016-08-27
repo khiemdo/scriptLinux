721dde275665f40855a22f39f985b8bef9bb0e8c
khiemdo <khiemdo@hopetechnik.com>
2016-08-28 01:32:44 +0800

    done with gogDocker.sh

--- scriptLinux/gogsInstall/gogsInstall.sh
+++ scriptLinux/gogsInstall/gogsInstall.sh
@@ -5,12 +5,16 @@ BASE_FOLDER=$1
 set -e 
 #install dependencies
 apt-get update
+apt-get install git wget nano elinks 
 
 adduser --disabled-login --gecos 'Gogs' git
 su - git
-cd ~
+cd /home/git
 mkdir local
-wget https://storage.googleapis.com/golang/go1.7.linux-armv6l.tar.gz
+
+export VERSION_GOLANG=go1.7.linux-armv6l
+export SOURCE_GOLANG=https://storage.googleapis.com/golang
+wget $SOURCE_GOLANG/$VERSION_GOLANG.tar.gz
 tar -C /home/git/local -xzf go*.tar.gz
 
 cd ~
@@ -22,6 +26,6 @@ source $HOME/.bashrc
 go get -u github.com/gogits/gogs
 cd $GOPATH/src/github.com/gogits/gogs
 go build
-
 cd $GOPATH/src/github.com/gogits/gogs
-./gogs web
+
+exit
