f6c77784dfd093c840e7fb391775ed961a53694d
khiemdo <khiemdo@hopetechnik.com>
2016-09-03 22:59:19 +0800

    toSync

--- scriptLinux/gogsInstall/gogsInstall.sh
+++ scriptLinux/gogsInstall/gogsInstall.sh
@@ -4,20 +4,33 @@
 BASE_FOLDER=$1
 #stop and exit on first error
 set -e 
+
 #install dependencies
 apt-get update
 apt-get install git wget nano  
 
 ROOT_MYSQL_PASSWD='root'
 SQL_SCRIPT_FILE='gogs.sql'
+service mysql start
 mysql -u root -p$ROOT_MYSQL_PASSWD < $SQL_SCRIPT_FILE
 
-adduser --disabled-login --gecos 'Gogs' git
+if !id "git" >/dev/null 2>&1;
+then
+	echo 'git user does not exist. create git user'
+	adduser --disabled-login --gecos 'Gogs' git
+fi
 su - git
 cd /home/git
-mkdir local
+mkdir -p local
 
-export VERSION_GOLANG=go1.7.linux-armv6l
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'armv7l' ]; then
+	export VERSION_GOLANG=go1.7.linux-armv6l
+elif [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	export VERSION_GOLANG=go1.7.linux-amd64
+fi
+echo "set VERSION_GOLANG to $VERSION_GOLANG"
+
 export SOURCE_GOLANG=https://storage.googleapis.com/golang
 wget $SOURCE_GOLANG/$VERSION_GOLANG.tar.gz
 tar -C /home/git/local -xzf go*.tar.gz
@@ -31,6 +44,7 @@ source $HOME/.bashrc
 go get -u github.com/gogits/gogs
 cd $GOPATH/src/github.com/gogits/gogs
 go build
+
 cd $GOPATH/src/github.com/gogits/gogs/scripts/systemd/gogs.service /lib/systemd/system
 systemctl enable gogs.service
 systemctl start gogs.service
