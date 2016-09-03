fb267425e844de68ecee513627cbec7a9f836d08
khiemdo <khiemdo@hopetechnik.com>
2016-09-04 01:19:15 +0800

    toSync

--- scriptLinux/mysql-server/mysqlServerInstall.py
+++ scriptLinux/mysql-server/mysqlServerInstall.py
@@ -24,9 +24,9 @@ def InstallMySqlServerUsingPExpect(logger, password):
     child = pexpect.spawn('apt-get -y install mysql-server')
     child.logfile = sys.stdout
     child.expect ('New password for the MySQL "root" user:')
-    child.sendline (password)
+    child.sendline (password+'\r')
     child.expect ('Repeat password for the MySQL "root" user:')
-    child.sendline (password)
+    child.sendline (password+'\r')
     child.expect(pexpect.EOF)
     logger.info("End InstallMySqlServerUsingPExpect")
 
